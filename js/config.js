365e5c74c45b0848f26f3a435f2113d9ed8f5444
Arif Kurniawan <52802394+arla27@users.noreply.github.com>
2026-09-22 11:06:06 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const SHEET_URL="https://script.google.com/macros/s/AKfycbwVEUzW1_bVuVOjRqaxI3EZ5svc70wsK5ZneIDBxcT2EFFDaWfJsc6bnYdLnUWUbLg0/exec";
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbwVEUzW1_bVuVOjRqaxI3EZ5svc70wsK5ZneIDBxcT2EFFDaWfJsc6bnYdLnUWUbLg0/exec";
